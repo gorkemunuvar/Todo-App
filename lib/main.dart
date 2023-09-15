2972abf99c50d96b563f0ec39976ffafd89e5543
Görkem <a.gorkemunuvar@gmail.com>
2023-09-15 11:02:21 +0200

    refactor: delete unneccesary platform folders

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,8 @@ class MyWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      home: Scaffold(),
+    );
   }
 }
