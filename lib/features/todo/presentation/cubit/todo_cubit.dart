4c73d2b50059f3c23339407c6c37e6764c64d3cb
Görkem <a.gorkemunuvar@gmail.com>
2023-09-16 15:20:11 +0200

    fix: todo id is null with crud operations

--- lib/features/todo/presentation/cubit/todo_cubit.dart
+++ lib/features/todo/presentation/cubit/todo_cubit.dart
@@ -24,26 +24,13 @@ class TodoCubit extends Cubit<TodoState> {
     await _fetchTodos();
   }
 
-  Future<void> _fetchTodos() async {
-    emit(state.copyWith(fetchingStatus: TodoStateStatus.loading));
-
-    final failureOrTodos = await _getTodosUseCase();
-    failureOrTodos.fold(
-      (failure) => emit(state.copyWith(fetchingStatus: TodoStateStatus.failure)),
-      (todos) => emit(state.copyWith(fetchingStatus: TodoStateStatus.success, todos: todos)),
-    );
-  }
-
   Future<void> createTodo(Todo todo) async {
     emit(state.copyWith(creatingStatus: TodoStateStatus.loading));
 
     final failureOrTodo = await _createTodoUseCase(todo);
     failureOrTodo.fold(
       (failure) => emit(state.copyWith(creatingStatus: TodoStateStatus.failure)),
-      (success) {
-        final todos = <Todo>[todo, ...state.todos ?? []];
-        emit(state.copyWith(creatingStatus: TodoStateStatus.success, todos: todos));
-      },
+      (success) => _fetchTodos(),
     );
   }
 
@@ -53,10 +40,7 @@ class TodoCubit extends Cubit<TodoState> {
     final failureOrTodo = await _deleteTodoUseCase(todoId);
     failureOrTodo.fold(
       (failure) => emit(state.copyWith(deletingStatus: TodoStateStatus.failure)),
-      (success) {
-        final todos = state.todos!.where((todo) => todo.id != todoId).toList();
-        emit(state.copyWith(deletingStatus: TodoStateStatus.success, todos: todos));
-      },
+      (success) => _fetchTodos(),
     );
   }
 
@@ -66,12 +50,17 @@ class TodoCubit extends Cubit<TodoState> {
     final failureOrTodo = await _updateTodoUseCase(todoId, isCompleted);
     failureOrTodo.fold(
       (failure) => emit(state.copyWith(updatingStatus: TodoStateStatus.failure)),
-      (success) {
-        final todos = state.todos!.map((todo) {
-          return todo.id == todoId ? todo.copyWith(isCompleted: isCompleted) : todo;
-        }).toList();
-        emit(state.copyWith(updatingStatus: TodoStateStatus.success, todos: todos));
-      },
+      (success) => _fetchTodos(),
+    );
+  }
+
+  Future<void> _fetchTodos() async {
+    emit(state.copyWith(fetchingStatus: TodoStateStatus.loading));
+
+    final failureOrTodos = await _getTodosUseCase();
+    failureOrTodos.fold(
+      (failure) => emit(state.copyWith(fetchingStatus: TodoStateStatus.failure)),
+      (todos) => emit(state.copyWith(fetchingStatus: TodoStateStatus.success, todos: todos)),
     );
   }
 }
